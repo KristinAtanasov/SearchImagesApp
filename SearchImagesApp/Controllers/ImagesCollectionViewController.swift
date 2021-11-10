19476f6793b54f078da99ec595d5ca6fa8433d7e
Chriss Chris <zenskech@outlook.com>
2021-11-10 16:18:28 +0500

    correcting selectedImageCell property

--- SearchImagesApp/Controllers/ImagesCollectionViewController.swift
+++ SearchImagesApp/Controllers/ImagesCollectionViewController.swift
@@ -8,13 +8,13 @@ class ImagesCollectionViewController: UICollectionViewController {
     private let reuseIdentifier = "Cell"
     
     var resultURLS = [Result]()
-    var selectedImageCell: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.setCollectionViewLayout(generateLayout(), animated: false)
     }
+    
     
     private func generateLayout() -> UICollectionViewLayout{
         let spacing: CGFloat = 5
@@ -66,19 +66,20 @@ class ImagesCollectionViewController: UICollectionViewController {
     }
     
     
-    // MARK: UICollectionViewDataSource
+    
+    // MARK: - UICollectionViewDataSource
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
-    
+   
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         return resultURLS.count
     }
     
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ImageCollectionViewCell
         
         let cellImageURL = resultURLS[indexPath.row].urls.regular
@@ -87,5 +88,4 @@ class ImagesCollectionViewController: UICollectionViewController {
         
         return cell
     }
-
 }
