39ce62910b22270c92217d7cc8f92d006a0cac15
Chriss Chris <zenskech@outlook.com>
2021-11-10 16:19:23 +0500

    editing spacing in the project

--- SearchImagesApp/Controllers/ImagesCollectionViewController.swift
+++ SearchImagesApp/Controllers/ImagesCollectionViewController.swift
@@ -9,12 +9,12 @@ class ImagesCollectionViewController: UICollectionViewController {
     
     var resultURLS = [Result]()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.setCollectionViewLayout(generateLayout(), animated: false)
     }
-    
     
     private func generateLayout() -> UICollectionViewLayout{
         let spacing: CGFloat = 5
@@ -62,7 +62,7 @@ class ImagesCollectionViewController: UICollectionViewController {
         else{
             return ShowSelectedImageViewController(coder: coder)
         }
-   
+        
     }
     
     
@@ -73,13 +73,13 @@ class ImagesCollectionViewController: UICollectionViewController {
         return 1
     }
     
-   
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
         return resultURLS.count
     }
     
-   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ImageCollectionViewCell
         
         let cellImageURL = resultURLS[indexPath.row].urls.regular
